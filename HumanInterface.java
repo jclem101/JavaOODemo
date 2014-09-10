c02ea64d8d9c73871cee7878803c5e636bb80f8e
jclem101 <jclem101@gmail.com>
2014-09-10 00:11:46 -0700

    Create HumanInterface.java

---
+++ HumanInterface.java
@@ -0,0 +1,10 @@
+package oo_demo_pkg;
+
+public interface HumanInterface{
+//humans physical interface to other humans
+	public abstract void emote();
+	public abstract void speak();
+	public abstract void move();
+	
+	
+}
